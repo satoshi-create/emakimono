3013efa00c8f86cae17a316aed4d937d07951e4c
satoshi <mimizunobouken@gmail.com>
2022-06-25 16:34:30 +0900

    add ekotoba

---
+++ js/ekotoba.js
@@ -0,0 +1,10 @@
+// $(function () {
+//   const sections = document.querySelectorAll(".section");
+//   console.log(sections);
+//   sections.forEach(function (section) {
+//     const btn = section.querySelector(".translate-btn.section-btn");
+//     btn.addEventListener("click", function () {
+//       section.classList.toggle("show-text");
+//     });
+//   });
+// });
