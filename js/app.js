ca77d5ee43d375cb3ed73de32dda380d8442a0b2
satoshi <mimizunobouken@gmail.com>
2022-07-22 20:25:38 +0900

    add about.html

--- js/app.js
+++ js/app.js
@@ -1,57 +1,30 @@
 import data from "../data/data.js";
 
 $(function () {
-  const contentsList = document.querySelector(".conteiner-list");
-  window.showMore = () => {
-    console.log("test");
-  };
-  contentsList.innerHTML = data
-    .map((item, index) => {
-      const {
-        id,
-        era,
-        gif,
-        thumb,
-        desc,
-        title,
-        edition,
-        author,
-        video,
-        readMore,
-        showMore,
-      } = item;
-      return `
-      <div class="emaki-card">
-      <div class="single-img-container">
-      <img class="single-img-thumb"src=${thumb}>
-      <img class="single-img-gif" src=${gif}>
-      <a href=${video} target="_blank" class="video-icon">
-      <i class="fa-brands fa-youtube"></i>
-      </a>
-      <a href="emaki.html?id=${index}" target="_blank" class="emaki-icon">
-      <svg  class="scroll" width="100%" height="100%" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M160,256L128,256C75.12,256 32,217.778 32,170.667L32,412.444C32,467.449 82.13,512 144,512C152.875,512 160,505.667 160,497.778L160,256ZM131.4,227.556L192,227.556L192,426.667L384,426.667C437,426.667 480,388.444 480,341.333L480,99.2C466.62,108.267 450.12,113.778 432,113.778L128,113.778C89.12,113.778 58.37,144.551 64.87,180.222C70,208.267 99.5,227.556 131.4,227.556ZM358,255.996L256,308.996L256,202.996L358,255.996ZM480,42.667C480,19.111 458.5,-0 432,-0L368,-0C359.1,-0 352,6.333 352,14.222L352,85.333L432,85.333C458.5,85.333 480,66.222 480,42.667Z"/>
-</svg>
-      </a>
-      </div>
-      <div class="emaki-info">
-      <div class="emaki-header">
-      <h3>${title} ${edition ? edition : ""}</h3>
-      <h4 class="author">${author}</h4>
-      <h4 class="era">${era}</h4>
-      </div>
-      <div class="emaki-desc">
-      <p>${desc}</p> 
-      </div>
-      </div>
-      </div>  
-    </a>
-    `;
-    })
-    .join("");
-  $(".emaki-desc p").readmore({
-    collapsedHeight: 100,
-    moreLink: '<a class="btn" href="#"><span>...続きを読む</span></a>',
-    lessLink: '<a class="btn btn_close" href="#"><span>...閉じる</span></a>',
+  // footer
+  const year = new Date().getFullYear();
+  const footer = document.querySelector(".footer");
+  footer.innerHTML = `<p>@${year} 横スクロールで楽しむ絵巻物 all right reserved</p> `;
+
+  
+  const navToggle = document.querySelector(".nav-toggle");
+  const links = document.querySelector(".links");
+
+  navToggle.addEventListener("click", function () {
+    links.classList.toggle("show-links");
+  });
+
+  // fixed navbar
+  const navbar = document.querySelector("nav");
+
+  window.addEventListener("scroll", function () {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+      navbar.classList.add("fixed-nav");
+      console.log("helo");
+    } else {
+      navbar.classList.remove("fixed-nav");
+    }
   });
 });
