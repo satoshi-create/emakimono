edffd2e756686c44bd33ab5abcc8a846b818eacb
satoshi <mimizunobouken@gmail.com>
2022-08-02 03:20:58 +0900

    data.js
    add srcFirstView

--- js/contentsList.js
+++ js/contentsList.js
@@ -1,11 +1,11 @@
 import data from "../data/data.js";
 
 $(function () {
-  document.querySelector("head").insertAdjacentHTML('afterend', data.map((item) => {
-    return `
-    <link rel="preload" as="image" href=${item.thumb}></link>
-    `;
-  }).join(""));
+  // document.querySelector("head").insertAdjacentHTML('afterend', data.map((item) => {
+  //   return `
+  //   <link rel="preload" as="image" href=${item.thumb}></link>
+  //   `;
+  // }).join(""));
 
   // document.querySelector("head").innerHTML = data.map((item) => {
   //   return `
