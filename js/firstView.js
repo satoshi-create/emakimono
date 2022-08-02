d19621f13f5e9ed3b793cc5a8f1d8dbfd5c50ddb
satoshi <mimizunobouken@gmail.com>
2022-08-02 11:31:36 +0900

    fixed firstView.js

--- js/firstView.js
+++ js/firstView.js
@@ -1,7 +1,30 @@
-import data from "../data/data.js";
+import datas from "../data/data.js";
 
-  document.querySelector("head").innerHTML = data.map((item) => {
+const params = new URLSearchParams(window.location.search);
+// console.log(window.location.hash);
+// const hash = window.location.hash.replace("#", "");
+// console.log(hash);
+const id = params.get("id");
+// import datas from "../data/${hash}.js";
+// console.log(datas);
+const data = datas[id];
+const {
+  title,
+  author,
+  edition,
+  emakis,
+  backgroundImage,
+  metadesc,
+  type,
+  srcFirstView,
+} = data;
+
+document.querySelector("head").innerHTML = datas
+  .map((item) => {
     return `
     <link rel="preload" as="image" href=${item.thumb}></link>
     `;
-  }).join("");
+  })
+  .join("");
+
+document.querySelector('link[rel="preload"]').href = `${srcFirstView}`;
